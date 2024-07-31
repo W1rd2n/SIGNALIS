60086cfc43957a39eca18f7ddc2585f05189ee73
Patryk Porebski <patryk.porebski.07@gmail.com>
2024-07-31 20:59:28 +0200

    rework of gallery component, change to characters component tables, home minor placement changes

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -5,14 +5,27 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.css',
+  styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent implements OnInit {
   public numberArray: number[] = [];
 
   ngOnInit(): void {
-    for (var i = 1; i <= 46; i++) {
+    for (var i = 1; i <= 44; i++) {
       this.numberArray.push(i);
     }
+  }
+
+  openModal(imageName: string) {
+    const modal = document.getElementById('myModal') as HTMLDivElement;
+    const modalImg = document.getElementById('img01') as HTMLImageElement;
+
+    modal.style.display = 'block';
+    modalImg.src = `../../assets/gallery/${imageName}`;
+  }
+
+  closeModal() {
+    const modal = document.getElementById('myModal') as HTMLDivElement;
+    modal.style.display = 'none';
   }
 }
